c7848a623e57f5a83a782821696c1c74eea9f17c
hanya411016 <61785348+hanya411016@users.noreply.github.com>
2021-07-19 22:00:52 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+var s = skrollr.init();
+// document.body.id = "skrollr-body";
 $(window).scroll(function(evt){
   if($(window).scrollTop() > 0)
     $(".navbar").removeClass("navbar-top");
   else
     $(".navbar").addClass("navbar-top");
 })
-
-var s = skrollr.init();
-document.body.id = "skrollr-body"
